0b43548bb1902ac9dc668216cb1ca607bee2fd9e
diogo <a233894432@gmail.com>
2016-07-01 17:21:24 +0800

    学习 interfaces.go
    学习 interfaces.go

---
+++ src/Interfaces-ex01.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+)
+
+func PrintAll(vals []interface{}) {
+	for _, val := range vals {
+		fmt.Println(val)
+	}
+}
+
+func main() {
+	names := []string{"stanley", "david", "oscar"}
+	//	vals := make([]interface{}, len(names))
+	//	for i, v := range names {
+	//		vals[i] = v
+	//	}
+	//	PrintAll(vals)
+	fmt.Println(names[0])
+}
