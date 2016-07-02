3329dfe8ce4d40c32b3a17ef8f28e6aadb088f4d
Diogoxiang <a233894432@gmail.com>
2016-07-03 00:28:40 +0800

    test 01
    test 01

--- src/main.go
+++ src/main.go
@@ -17,24 +17,9 @@ type Job struct {
 }
 
 func main() {
-	iris.Config.Render.Template.Extensions = []string{".jade"}
-	// this is optionally, you can keep .html extension
-	iris.Config.Render.Template.Engine = iris.JadeEngine
-
-	iris.Get("/", func(ctx *iris.Context) {
-
-		job1 := Job{Employer: "Super Employer", Role: "Team leader"}
-		job2 := Job{Employer: "Fast Employer", Role: "Project managment"}
-
-		person := Person{
-			Name:   "name1",
-			Age:    50,
-			Emails: []string{"email1@something.gr", "email2.anything@gmail.com"},
-			Jobs:   []*Job{&job1, &job2},
-		}
-
-		ctx.MustRender("page.jade", person)
+	iris.Get("/hi", func(ctx *iris.Context) {
+		ctx.Write("Hi %s", "iris")
 	})
-
 	iris.Listen(":8080")
+	//	err := iris.ListenWithErr(":8080")
 }
