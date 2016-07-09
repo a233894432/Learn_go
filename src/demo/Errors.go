81739a7480c4cbea98ca88818b8214f8b9affce9
Diogoxiang <a233894432@gmail.com>
2016-07-10 00:18:46 +0800

    理解  Errors.
    理解  Errors.

--- src/demo/Errors.go
+++ src/demo/Errors.go
@@ -32,21 +32,21 @@ func f2(arg int) (int, error) {
 }
 
 func main() {
-	//	for _, i := range []int{7, 42} {
-	//		if r, e := f1(i); e != nil {
-	//			fmt.Println("f1 failed:", e)
-	//		} else {
-	//			fmt.Println("f1 worked:", r)
-	//		}
-	//	}
+	for _, i := range []int{7, 42} {
+		if r, e := f1(i); e != nil {
+			fmt.Println("f1 failed:", e)
+		} else {
+			fmt.Println("f1 worked:", r)
+		}
+	}
 
-	//	for _, i := range []int{7, 42} {
-	//		if r, e := f2(i); e != nil {
-	//			fmt.Println("f2 failed:", e)
-	//		} else {
-	//			fmt.Println("f2 worked:", r)
-	//		}
-	//	}
+	for _, i := range []int{7, 42} {
+		if r, e := f2(i); e != nil {
+			fmt.Println("f2 failed:", e)
+		} else {
+			fmt.Println("f2 worked:", r)
+		}
+	}
 
 	_, e := f2(42)
 	if ae, ok := e.(*argError); ok {
