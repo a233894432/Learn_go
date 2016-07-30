804c28635cc8eacbe13c3d70c4ad7ecb42bc79d9
Diogoxiang <a233894432@gmail.com>
2016-07-31 01:38:46 +0800

    ,,,..

---
+++ src/demo/local_scope.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+var a string
+
+func main() {
+	a = "G"
+	fmt.Println(a)
+	f1()
+
+}
+
+func f1() {
+	a := "ooo"
+	fmt.Println(a)
+	f2()
+}
+
+func f2() {
+	fmt.Println(a)
+}
