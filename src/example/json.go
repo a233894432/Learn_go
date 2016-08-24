552bc99077c158b48328cf8390d4d8ae77eb8a31
Diogoxiang <a233894432@gmail.com>
2016-08-24 23:28:09 +0800

    学习 JSON
    JSON

--- src/example/json.go
+++ src/example/json.go
@@ -31,8 +31,13 @@ func main() {
 	vc := VCard{"Jan", "Kersschot", []*Address{pa, wa}, "noneE"}
 	// fmt.Printf("%v: \n", vc) // {Jan Kersschot [0x126d2b80 0x126d2be0] none}:
 	// JSON format:
-	js, _ := json.Marshal(vc)
-	fmt.Printf("JSON format: %s", js)
+	js, _ := json.MarshalIndent(vc," ","  ")
+	//格式化数据
+	fmt.Printf("JSON format: %s \n", js)
+	//单行数据
+	js01,_:=json.Marshal(vc)
+	fmt.Printf("JSON line: %s",js01)
+
 	// using an encoder:
 	file, _ := os.OpenFile("src/example/vcard.json", os.O_CREATE|os.O_WRONLY, 0666)
 	defer file.Close()
